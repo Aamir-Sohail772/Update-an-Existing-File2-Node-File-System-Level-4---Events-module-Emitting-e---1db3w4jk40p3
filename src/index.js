ef4123f069f844c03ec323359125325ce560f9e9
Aamir Sohail <80810119+Aamir-Sohail772@users.noreply.github.com>
2023-09-05 12:10:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ const fileName = 'myfile.txt';
 const updateFile = async (fileName, fileContent) => {
     //Write your code here to overwrite the file content
     //Don't change function name
+    try {
+    // Read the existing content of the file
+    const existingContent = await fs.readFile(fileName, "utf8");
+
+    // Concatenate the existing content with the new content to be added
+    const updatedContent = existingContent + fileContent;
+
+    // Write the updated content back to the file
+    await fs.writeFile(fileName, updatedContent, "utf8");
+
+    return updatedContent;
+  } catch (err) {
+    // If there's an error, return the error message
+    return err.message;
+  }
     
 };
 
